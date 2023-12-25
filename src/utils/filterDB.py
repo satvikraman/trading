b9b8609a226b3937881a581b1574e02512735f7c
Satvik Raman <satvikraman11@gmail.com>
2023-12-25 10:43:57 +0530

    [Utils] + Add a utility filterDB.py to filter the trades on a certain criteria. This is being used to filter the trades made by Satvik

---
+++ src/utils/filterDB.py
@@ -0,0 +1,48 @@
+from typing import Any
+import os
+import re
+import shutil
+import sys
+import datetime
+import configparser
+
+sys.path.append('./src/common')
+
+from persistence import persistence
+
+class app():
+    def __init__(self, configFile, db=None, dryRun=False):
+        if os.path.isfile(configFile):
+            self.__config = configparser.ConfigParser()
+            self.__config.read(configFile)
+            db = self.__config['DATABASE']['DB']            
+            self.backupDb(db)
+            self.__persistence = persistence(configFile, db)
+
+
+    def backupDb(self, db):
+        backupDb = db + '-FILTER-' + datetime.datetime.today().strftime("%d-%b-%Y-%H-%M-%S")
+        shutil.copyfile(db, backupDb)
+
+
+    def filterDb(self, source, filterDate):
+        dbDicts = self.__persistence.getDb([['SOURCE', source]])
+        for dbDict in dbDicts:
+            for orderDict in dbDict['OPEN_ORDERS'] + dbDict['CLOSE_ORDERS']:
+                if filterDate.strftime("%d-%b-%Y") in orderDict['CREATE_TIME'] and orderDict['TRADED_QTY'] > 0:
+                    print('DATE: ', filterDate, 'STOCK: ', 'Tx: ', orderDict['BUY_SELL'], dbDict['NSE_SYMBOL'], orderDict)
+
+
+if __name__ == '__main__':
+    # Backup DB. We will work on the original DB
+    filter = app('./payTmMoney.ini')
+
+    start = datetime.date(2023, 12, 17)
+    end = datetime.date(2023, 12, 24)
+    filterDate = start
+    while filterDate <= end:
+        print("Filtering txs on : ", filterDate)
+        filter.filterDb('SRMomentum', filterDate)
+        filterDate += datetime.timedelta(days=1)
+        print("----")
+
