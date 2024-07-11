6bc4a33f37b6281fbb655a01f579ec6ca955c45a
Satvik Raman <satvikraman11@gmail.com>
2024-07-11 19:42:04 +0530

    Checking in the geckodriver for firefox

---
+++ appIciciBreezeBroker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Change directory to where the Python script is located
+cd /home/arvind/trading/trading
+# Activate the Anaconda environment
+source ../trd_chrome/bin/activate
+
+# Function to get current hour and minute
+get_hour() {
+    date "+%H"
+}
+
+get_minute() {
+    date "+%M"
+}
+
+# Main loop
+while true; do
+    python src/icici/appIciciBreeze.py
+    sleep 5
+
+    HOUR=$(get_hour)
+    MIN=$(get_minute)
+
+    if [ $HOUR -lt 15 ]; then
+        echo "Restarting appIciciBreeze at $HOUR:$MIN"
+    elif [ $HOUR -eq 15 ] && [ $MIN -le 30 ]; then
+        echo "Restarting appIciciBreeze at $HOUR:$MIN"
+    else
+        break
+    fi
+done
